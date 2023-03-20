1c8ca96ff0f497416dcc6e948e71bf5513d9335e
Alessandro Moscatelli <alessandro.moscatelli@live.com>
2023-03-20 02:27:50 +0100

    automagical reflection registration of nosql entities

--- core-parent/deployment/src/main/java/io/quarkiverse/jnosql/core/deployment/Processor.java
+++ core-parent/deployment/src/main/java/io/quarkiverse/jnosql/core/deployment/Processor.java
@@ -1,10 +1,18 @@
 package io.quarkiverse.jnosql.core.deployment;
 
+import static org.jboss.jandex.AnnotationTarget.Kind.CLASS;
+
+import jakarta.nosql.Entity;
+
+import org.jboss.jandex.Type;
+
 import io.quarkiverse.jnosql.core.runtime.QuarkusEntityMetadataExtension;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveHierarchyBuildItem;
 
 class Processor {
 
@@ -13,6 +21,16 @@ class Processor {
     @BuildStep
     FeatureBuildItem feature() {
         return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    void build(CombinedIndexBuildItem index, BuildProducer<ReflectiveHierarchyBuildItem> reflectiveHierarchyBuildItemProducer) {
+        index.getIndex().getAnnotations(Entity.class).stream().filter(
+                annotationIntance -> CLASS.equals(annotationIntance.target().kind())).forEach(
+                        annotationIntance -> reflectiveHierarchyBuildItemProducer.produce(
+                                new ReflectiveHierarchyBuildItem.Builder().type(Type.create(
+                                        annotationIntance.target().asClass().name(),
+                                        Type.Kind.CLASS)).build()));
     }
 
     @BuildStep
