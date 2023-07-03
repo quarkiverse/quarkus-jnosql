e6dc133bce778047ee3ce74e9d282447c54cea51
Maximillian Arruda <dearrudam@gmail.com>
2023-07-03 06:59:49 -0300

    feat: added column-parent/deployment and column-parent/ modules
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

---
+++ column-parent/deployment/src/main/java/io/quarkiverse/jnosql/column/deployment/Processor.java
@@ -0,0 +1,23 @@
+package io.quarkiverse.jnosql.column.deployment;
+
+import io.quarkiverse.jnosql.column.runtime.ColumnManagerProducer;
+import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+
+class Processor {
+
+    private static final String FEATURE = "jnosql-column";
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    void build(BuildProducer<AdditionalBeanBuildItem> additionalBeanProducer) {
+        additionalBeanProducer.produce(AdditionalBeanBuildItem.unremovableOf(ColumnManagerProducer.class));
+    }
+
+}
