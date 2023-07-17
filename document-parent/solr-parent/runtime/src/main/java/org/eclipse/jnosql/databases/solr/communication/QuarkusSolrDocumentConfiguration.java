b5347c026de69a4affda4573d8297e7566bf139e
A.Moscatelli <A.Moscatelli@AC-AMOSCATELLI1.itmaster.local>
2023-07-17 23:20:26 +0200

    solr integration

---
+++ document-parent/solr-parent/runtime/src/main/java/org/eclipse/jnosql/databases/solr/communication/QuarkusSolrDocumentConfiguration.java
@@ -0,0 +1,7 @@
+package org.eclipse.jnosql.databases.solr.communication;
+
+import jakarta.inject.Singleton;
+
+@Singleton
+public class QuarkusSolrDocumentConfiguration extends SolrDocumentConfiguration {
+}
--- keyvalue-parent/runtime/src/main/java/io/quarkiverse/jnosql/keyvalue/runtime/BucketManagerProducer.java
+++ keyvalue-parent/runtime/src/main/java/io/quarkiverse/jnosql/keyvalue/runtime/BucketManagerProducer.java
@@ -1,34 +1,21 @@
 package io.quarkiverse.jnosql.keyvalue.runtime;
 
-import static org.eclipse.jnosql.mapping.config.MappingConfigurations.KEY_VALUE_DATABASE;
-
-import java.util.Optional;
 import java.util.function.Supplier;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import jakarta.annotation.Priority;
-import jakarta.data.exceptions.MappingException;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Alternative;
 import jakarta.enterprise.inject.Produces;
-import jakarta.inject.Inject;
 
-import org.eclipse.jnosql.communication.Settings;
 import org.eclipse.jnosql.communication.keyvalue.BucketManager;
 import org.eclipse.jnosql.communication.keyvalue.BucketManagerFactory;
 import org.eclipse.jnosql.communication.keyvalue.KeyValueConfiguration;
+import org.eclipse.jnosql.mapping.config.MappingConfigurations;
 
-import io.quarkiverse.jnosql.core.runtime.MicroProfileSettings;
+import io.quarkiverse.jnosql.core.runtime.AbstractManagerProducer;
 
-public class BucketManagerProducer implements Supplier<BucketManager> {
-
-    private static final Logger LOGGER = Logger.getLogger(BucketManagerProducer.class.getName());
-
-    private final Settings settings = new MicroProfileSettings();
-
-    @Inject
-    private KeyValueConfiguration documentConfiguration;
+public class BucketManagerProducer extends AbstractManagerProducer<BucketManager, BucketManagerFactory, KeyValueConfiguration>
+        implements Supplier<BucketManager> {
 
     @Override
     @Produces
@@ -36,16 +23,7 @@ public class BucketManagerProducer implements Supplier<BucketManager> {
     @Priority(1)
     @ApplicationScoped
     public BucketManager get() {
-        BucketManagerFactory managerFactory = documentConfiguration.apply(settings);
-
-        Optional<String> database = settings.get(KEY_VALUE_DATABASE, String.class);
-        String db = database.orElseThrow(() -> new MappingException("Please, inform the database filling up the property "
-                + KEY_VALUE_DATABASE));
-        BucketManager manager = managerFactory.apply(db);
-
-        LOGGER.log(Level.FINEST, "Starting  a BucketManager instance using Eclipse MicroProfile Config, database name: {0}",
-                db);
-        return manager;
+        return apply(MappingConfigurations.KEY_VALUE_DATABASE);
     }
 
 }
