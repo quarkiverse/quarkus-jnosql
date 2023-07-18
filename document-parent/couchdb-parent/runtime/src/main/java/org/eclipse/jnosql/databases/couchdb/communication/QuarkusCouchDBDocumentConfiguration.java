6ad509af277c1f3fbefa09bea27e3f496d7c1d1d
A.Moscatelli <A.Moscatelli@AC-AMOSCATELLI1.itmaster.local>
2023-07-18 14:31:38 +0200

    couchdb integration

---
+++ document-parent/couchdb-parent/runtime/src/main/java/org/eclipse/jnosql/databases/couchdb/communication/QuarkusCouchDBDocumentConfiguration.java
@@ -0,0 +1,7 @@
+package org.eclipse.jnosql.databases.couchdb.communication;
+
+import jakarta.inject.Singleton;
+
+@Singleton
+public class QuarkusCouchDBDocumentConfiguration extends CouchDBDocumentConfiguration {
+}
