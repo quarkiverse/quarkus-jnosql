4ad560129f369f190e25cc691c0e6017ade74cc7
Maximillian Arruda <dearrudam@gmail.com>
2023-07-18 02:57:16 -0300

    tests: remove unnecessary IT class by now
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- keyvalue-parent/couchbase-parent/integration-tests/src/test/java/io/quarkiverse/jnosql/keyvalue/couchbase/it/JNoSQLResourceIT.java
+++
@@ -1,7 +0,0 @@
-package io.quarkiverse.jnosql.keyvalue.couchbase.it;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class JNoSQLResourceIT extends JNoSQLResourceTest {
-}
