05df7b68929baabca3103394d539401ac4677919
Maximillian Arruda <dearrudam@gmail.com>
2023-07-18 02:07:22 -0300

    refactor: add warn message during native-compilation
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- keyvalue-parent/couchbase-parent/deployment/src/main/java/io/quarkiverse/jnosql/keyvalue/couchbase/deployment/Processor.java
+++ keyvalue-parent/couchbase-parent/deployment/src/main/java/io/quarkiverse/jnosql/keyvalue/couchbase/deployment/Processor.java
@@ -1,19 +1,36 @@
 package io.quarkiverse.jnosql.keyvalue.couchbase.deployment;
 
+import io.quarkus.deployment.annotations.ExecutionTime;
+import io.quarkus.deployment.annotations.Record;
+import io.quarkus.deployment.pkg.steps.NativeBuild;
+import org.apache.camel.quarkus.core.JvmOnlyRecorder;
 import org.eclipse.jnosql.databases.couchbase.communication.QuarkusCouchbaseKeyValueConfiguration;
 
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import org.jboss.logging.Logger;
 
 class Processor {
+
+    private static final Logger LOG = Logger.getLogger(Processor.class);
 
     private static final String FEATURE = "jnosql-keyvalue-couchbase";
 
     @BuildStep
     FeatureBuildItem feature() {
         return new FeatureBuildItem(FEATURE);
+    }
+
+    /**
+     * Remove this once this extension starts supporting the native mode.
+     */
+    @BuildStep(onlyIf = NativeBuild.class)
+    @Record(value = ExecutionTime.RUNTIME_INIT)
+    void warnJvmInNative(JvmOnlyRecorder recorder) {
+        JvmOnlyRecorder.warnJvmInNative(LOG, FEATURE); // warn at build time
+        recorder.warnJvmInNative(FEATURE); // warn at runtime
     }
 
     @BuildStep
