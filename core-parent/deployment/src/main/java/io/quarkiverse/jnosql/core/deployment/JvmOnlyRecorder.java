2e7ef5819b42d76d4d19db8b737530dee3db1dd7
Maximillian Arruda <dearrudam@gmail.com>
2023-07-18 23:05:27 -0300

    chore: change warn message
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- core-parent/deployment/src/main/java/io/quarkiverse/jnosql/core/deployment/JvmOnlyRecorder.java
+++ core-parent/deployment/src/main/java/io/quarkiverse/jnosql/core/deployment/JvmOnlyRecorder.java
@@ -11,7 +11,9 @@ public class JvmOnlyRecorder {
     }
 
     public static void warnJvmInNative(Logger log, String feature) {
-        log.warnf("The %s extension was not tested in native mode. You may want to report about the success or failure running it in native mode on https://github.com/apache/camel-quarkus/issues?q=is%%3Aissue+%s", feature, feature.replace('-', '+'));
+        log.warnf(
+                "The %s extension was not tested in native mode. You may want to report about the success or failure running it in native mode on https://github.com/quarkiverse/quarkus-jnosql/issues?q=is:issue+%s",
+                feature, feature.replace('-', '+'));
     }
 
     public void warnJvmInNative(String feature) {
