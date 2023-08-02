d3678af62a6618a626d03cd18c6b51c1a951a378
Maximillian Arruda <dearrudam@gmail.com>
2023-08-02 09:16:13 -0300

    chore: upgrade jnosql.version to 1.0.1
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- column-parent/cassandra-parent/runtime/src/main/java/org/eclipse/jnosql/databases/cassandra/communication/QuarkusCassandraConfiguration.java
+++ column-parent/cassandra-parent/runtime/src/main/java/org/eclipse/jnosql/databases/cassandra/communication/QuarkusCassandraConfiguration.java
@@ -6,7 +6,6 @@ import jakarta.inject.Singleton;
 import org.eclipse.jnosql.communication.Settings;
 import org.eclipse.jnosql.communication.column.ColumnConfiguration;
 import org.eclipse.jnosql.communication.column.ColumnManagerFactory;
-import org.eclipse.microprofile.context.ManagedExecutor;
 
 import com.datastax.oss.quarkus.runtime.api.session.QuarkusCqlSession;
 
@@ -16,14 +15,10 @@ public class QuarkusCassandraConfiguration implements ColumnConfiguration {
     @Inject
     protected QuarkusCqlSession quarkusCqlSession;
 
-    @Inject
-    protected ManagedExecutor managedExecutor;
-
     @Override
     public ColumnManagerFactory apply(Settings settings) throws NullPointerException {
         return new QuarkusCassandraColumnManagerFactory(
-                quarkusCqlSession,
-                managedExecutor);
+                quarkusCqlSession);
     }
 
 }
