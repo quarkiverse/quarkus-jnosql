64e962f87964ea6099418ed0715b820bcd866355
Maximillian Arruda <dearrudam@gmail.com>
2023-10-08 00:35:30 -0300

    chore: upgrade jnosql.version to 1.0.2
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- core-parent/runtime/src/main/java/io/quarkiverse/jnosql/core/runtime/QuarkusEntityMetadataExtension.java
+++ core-parent/runtime/src/main/java/io/quarkiverse/jnosql/core/runtime/QuarkusEntityMetadataExtension.java
@@ -4,7 +4,7 @@ import jakarta.enterprise.event.Observes;
 import jakarta.enterprise.event.Startup;
 import jakarta.inject.Singleton;
 
-import org.eclipse.jnosql.mapping.reflection.EntityMetadataExtension;
+import org.eclipse.jnosql.mapping.spi.EntityMetadataExtension;
 
 @Singleton
 public class QuarkusEntityMetadataExtension extends EntityMetadataExtension {
