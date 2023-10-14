07da518868cd6211f60142ee12da38b2f7975930
Maximillian Arruda <dearrudam@gmail.com>
2023-10-14 04:05:52 -0300

    chore: removed unnecessary methods and fixed imports
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- core-parent/deployment/src/main/java/io/quarkiverse/jnosql/core/deployment/ServiceProviderRegister.java
+++ core-parent/deployment/src/main/java/io/quarkiverse/jnosql/core/deployment/ServiceProviderRegister.java
@@ -5,25 +5,23 @@ import io.quarkus.deployment.builditem.nativeimage.ServiceProviderBuildItem;
 import io.quarkus.deployment.util.ServiceUtil;
 
 import java.io.IOException;
-import java.util.function.Consumer;
 
 /**
  * Utility class to register service providers
  */
 public interface ServiceProviderRegister {
 
-
     /**
-      * Register a service provider for a given service interface.
-      *
-      * @param services the build producer
-      * @param serviceInterface the service interface
-      */
-     static void registerService(BuildProducer<ServiceProviderBuildItem> services, Class<?>... serviceInterface) {
-         for (Class<?> service : serviceInterface) {
-             registerService(services, service);
-         }
-     }
+     * Register a service provider for a given service interface.
+     *
+     * @param services the build producer
+     * @param serviceInterface the service interface
+     */
+    static void registerService(BuildProducer<ServiceProviderBuildItem> services, Class<?>... serviceInterface) {
+        for (Class<?> service : serviceInterface) {
+            registerService(services, service);
+        }
+    }
 
     /**
      * Register a service provider for a given service interface.
@@ -46,9 +44,5 @@ public interface ServiceProviderRegister {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    static Consumer<? super Class<?>> registerInto(BuildProducer<ServiceProviderBuildItem> services) {
-        return clazz -> registerService(services, clazz);
     }
 }
