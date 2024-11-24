e418c38ffd47c1e6c8e81e8701a58a4df6a7c2a8
Otavio Santana <otaviopolianasantana@gmail.com>
2024-11-24 18:54:53 +0000

    feat: remove unsed files at redis
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- keyvalue-parent/redis-parent/runtime/src/main/java/org/eclipse/jnosql/databases/redis/communication/QuarkusRedisKeyValueConfiguration.java
+++ keyvalue-parent/redis-parent/runtime/src/main/java/org/eclipse/jnosql/databases/redis/communication/QuarkusRedisKeyValueConfiguration.java
@@ -1,26 +1,18 @@
 package org.eclipse.jnosql.databases.redis.communication;
 
-import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
 import org.eclipse.jnosql.communication.Settings;
 import org.eclipse.jnosql.communication.keyvalue.KeyValueConfiguration;
 
-import io.vertx.mutiny.redis.client.RedisAPI;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-
 @Singleton
 public class QuarkusRedisKeyValueConfiguration implements KeyValueConfiguration {
 
-    @Inject
-    protected RedisAPI client;
+    private final RedisConfiguration configuration = new RedisConfiguration();
 
     @Override
     public RedisBucketManagerFactory apply(Settings settings) throws NullPointerException {
-        return new DefaultRedisBucketManagerFactory(
-                new JedisPool(
-                        new Jedis(client)));
+        return configuration.apply(settings);
     }
 
 }
