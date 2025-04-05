149de3bf8da9cd6474e97b59efb94cc37cccafbb
Maximillian Arruda <dearrudam@gmail.com>
2025-04-05 18:18:38 -0300

    chore: removed unnecessary logs

--- mongodb/runtime/src/main/java/org/eclipse/jnosql/databases/mongodb/communication/QuarkusMongoDBDatabaseManagerProducer.java
+++ mongodb/runtime/src/main/java/org/eclipse/jnosql/databases/mongodb/communication/QuarkusMongoDBDatabaseManagerProducer.java
@@ -1,8 +1,5 @@
 package org.eclipse.jnosql.databases.mongodb.communication;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import jakarta.annotation.Priority;
 import jakarta.enterprise.inject.Alternative;
 import jakarta.enterprise.inject.Default;
@@ -19,15 +16,12 @@ import io.quarkiverse.jnosql.core.runtime.AbstractDatabaseManagerProducer;
 public class QuarkusMongoDBDatabaseManagerProducer extends
         AbstractDatabaseManagerProducer<MongoDBDocumentManager, MongoDBDocumentManagerFactory, QuarkusMongoDBDocumentConfiguration> {
 
-    private static final Logger LOGGER = Logger.getLogger(QuarkusMongoDBDatabaseManagerProducer.class.getName());
-
     @Produces
     @Priority(1)
     @Alternative
     @Default
     @Database(DatabaseType.DOCUMENT)
     public MongoDBDocumentManager get() {
-        LOGGER.log(Level.FINEST, "creating a MongoDBDocumentManager");
         return get(MappingConfigurations.DOCUMENT_DATABASE);
     }
 
