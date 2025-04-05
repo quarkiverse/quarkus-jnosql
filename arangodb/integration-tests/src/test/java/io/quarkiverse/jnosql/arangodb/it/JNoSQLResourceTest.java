2206d676c33373dad4ddbadbbee883b34b025379
Maximillian Arruda <dearrudam@gmail.com>
2025-04-05 15:25:59 -0300

    refactor: refactored quarkus-jnosql-arangodb module

---
+++ arangodb/integration-tests/src/test/java/io/quarkiverse/jnosql/arangodb/it/JNoSQLResourceTest.java
@@ -0,0 +1,36 @@
+package io.quarkiverse.jnosql.arangodb.it;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+@QuarkusTestResource(ArangodbTestResource.class)
+@TestHTTPEndpoint(JNoSQLResource.class)
+public class JNoSQLResourceTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "/using-jakarta-nosql",
+            "/using-jakarta-nosql-record",
+            "/using-jakarta-data",
+            "/using-jakarta-data-record",
+    })
+    public void test(String path) {
+        given()
+                .when()
+                .get(path)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body(is(not(empty())));
+    }
+}
