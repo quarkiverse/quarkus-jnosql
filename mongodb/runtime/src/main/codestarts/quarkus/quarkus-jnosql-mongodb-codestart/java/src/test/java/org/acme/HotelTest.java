d4d632af0b2aac1d0ca8c7ed54cc88a631fa5180
Maximillian Arruda <dearrudam@gmail.com>
2025-06-01 05:33:40 -0300

    refactor: adding more methods to show more Jakarta Data capabilities
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- mongodb/runtime/src/main/codestarts/quarkus/quarkus-jnosql-mongodb-codestart/java/src/test/java/org/acme/HotelTest.java
+++ mongodb/runtime/src/main/codestarts/quarkus/quarkus-jnosql-mongodb-codestart/java/src/test/java/org/acme/HotelTest.java
@@ -4,15 +4,24 @@ import io.quarkus.test.junit.QuarkusTest;
 import jakarta.data.page.PageRequest;
 import jakarta.inject.Inject;
 import net.datafaker.Faker;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
+/**
+ * <p>HotelTest is a test class for testing the Hotel operations including check-in, check-out,
+ * and paginated room retrieval.</p>
+ *
+ * <p>It uses Quarkus Test framework and Faker library to generate random data for testing.</p>
+ */
 @QuarkusTest
 class HotelTest {
 
@@ -29,57 +38,83 @@ class HotelTest {
     }
 
     @Test
-    void testHelloEndpoint() {
-        // Given the hotel database is empty, we expect to get an empty list of rooms
+    @DisplayName("Test hotel operations including check-in, check-out, and paginated room retrieval")
+    void testHotelOperations() {
 
-        var room1 = new Room("101", new Guest(faker.passport().toString(), faker.name().fullName()));
-        var room2 = new Room("102", new Guest(faker.passport().toString(), faker.name().fullName()));
-        var room3 = new Room("103", new Guest(faker.passport().toString(), faker.name().fullName()));
+        assertThat(hotel.getCheckedInRooms(PageRequest.ofPage(1).size(2)))
+                .as("If no rooms are checked in, any paginated request should return an empty list.")
+                .isEmpty();
+
+        var room1 = new Room("101", new Guest(faker.passport().valid(), faker.name().fullName()));
+        var room2 = new Room("102", new Guest(faker.passport().valid(), faker.name().fullName()));
+        var room3 = new Room("103", new Guest(faker.passport().valid(), faker.name().fullName()));
 
         var rooms = List.of(room1, room2, room3);
 
-        assertThat(hotel.getRooms(PageRequest.ofPage(1).size(2)))
-                .as("should there's no rooms in the hotel database")
-                .isEmpty();
-
-        // Check that the hotel database is empty
         assertThat(hotel.checkIn(room1))
-                .as("should do the check-in of the room %s in the hotel database", room1)
+                .as("there should have been a check-in for room %s", room1)
                 .isEqualTo(room1);
         assertThat(hotel.checkIn(room2))
-                .as("should do the check-in of the room %s in the hotel database", room2)
+                .as("there should have been a check-in for room %s", room2)
                 .isEqualTo(room2);
         assertThat(hotel.checkIn(room3))
-                .as("should do the check-in ofk the room %s in the hotel database", room3)
+                .as("there should have been a check-in for room %s", room3)
                 .isEqualTo(room3);
 
-        assertThat(hotel.getRooms(PageRequest.ofPage(1).size(2)))
-                .as("should return %d of %d rooms from the first page of the paged query in the hotel database", 2, 2)
-                .hasSize(2);
+        assertThat(hotel.getCheckedInRooms(PageRequest.ofPage(1).size(2)))
+                .as("Given a paginated request for page %d with %d items per page," +
+                        " the response should return exactly %d checked-in rooms.", 1, 2, 2)
+                .hasSize(2)
+                .as("The response must include the checked-in rooms %s", Stream.of(room1, room2)
+                        .map(Room::number).collect(Collectors.joining(", and ")))
+                .containsExactly(room1, room2);
 
-        assertThat(hotel.getRooms(PageRequest.ofPage(2).size(2)))
-                .as("should return %d of %d rooms from the first page of the paged query in the hotel database", 1, 2)
-                .hasSize(1);
+        assertThat(hotel.getCheckedInRooms(PageRequest.ofPage(2).size(2)))
+                .as("When requesting page %d with %d items per page, " +
+                        "the response must not include rooms already listed on the previous page.", 2, 2)
+                .hasSize(1)
+                .as("The response must include the checked-in room %s", room3.number())
+                .containsExactly(room3);
 
-        assertThat(hotel.getRooms(PageRequest.ofPage(1).size(10)))
-                .as("should return %d of %d rooms from the first page of the paged query in the hotel database", rooms.size(), 10)
+        assertThat(hotel.getCheckedInRooms(PageRequest.ofPage(3).size(2)))
+                .as("When requesting page %d with %d items per page, " +
+                        "the result must include no checked-in rooms.", 3, 2)
+                .hasSize(0);
+
+        assertThat(hotel.getCheckedInRooms(PageRequest.ofPage(1).size(10)))
+                .as("Given a paginated request for page %d with %d items per page, " +
+                        "the response should return exactly %d checked-in rooms.", 1, 10, rooms.size())
                 .hasSize(rooms.size())
-                .as("should contains any of the checked-in rooms in the hotel database previously")
+                .as("The response must include the checked-in rooms %s.", rooms.stream()
+                        .map(Room::number).collect(Collectors.joining(", and ")))
                 .containsAll(rooms);
 
-        // Check that the hotel database is empty
-        Assertions.assertThatCode(() -> hotel.checkOut(room1))
-                .as("should check out the room %s in the hotel database", room1)
+        assertThatCode(() -> hotel.checkOut(room1))
+                .as("%s should be checked out in the hotel", room1)
                 .doesNotThrowAnyException();
 
         rooms = List.of(room2, room3);
 
-        assertThat(hotel.getRooms(PageRequest.ofPage(1).size(10)))
-                .as("should return %d of %d rooms from the first page of the paged query in the hotel database", rooms.size(), 10)
+        assertThat(hotel.getCheckedInRooms(PageRequest.ofPage(1).size(10)))
+                .as("Given a paginated request for page %d with %d items per page, " +
+                        "the response should return exactly %d checked-in rooms.", 1, 10, rooms.size())
                 .hasSize(rooms.size())
-                .as("should contains any of the checked-in rooms in the hotel database previously")
+                .as("The response must include the checked-in rooms %s.", rooms.stream()
+                        .map(Room::number).collect(Collectors.joining(", and ")))
                 .containsAll(rooms);
 
+        assertThat(hotel.getCheckedInRoomsByGuestDocument(room2.guest().document(), PageRequest.ofPage(1).size(2)))
+                .as("Given a paginated request for page %d with %d items per page for given a guest document, " +
+                        "the response should return exactly %d checked-in rooms.", 1, 2, 1)
+                .hasSize(1)
+                .as("The response must include the checked-in room %s", Stream.of(room2)
+                        .map(Room::number).collect(Collectors.joining(", and ")))
+                .containsExactly(room2);
+
+        assertThat(hotel.getCheckedInRoomsByGuestDocument(faker.passport().valid(), PageRequest.ofPage(1).size(2)))
+                .as("Given a paginated request for page %d with %d items per page for given a non registered guest document, " +
+                        "any paginated request should return an empty list.", 1, 2)
+                .isEmpty();
     }
 
 }
