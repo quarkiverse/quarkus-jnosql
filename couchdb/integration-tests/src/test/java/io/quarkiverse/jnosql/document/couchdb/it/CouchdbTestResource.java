a63f0f1d5e9da21497a21e77252ae7eb754cf2df
Maximillian Arruda <dearrudam@gmail.com>
2025-06-06 22:32:10 -0300

    refactor: refactored the codestart code for quarkus-jnosql-couchdb
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- couchdb/integration-tests/src/test/java/io/quarkiverse/jnosql/document/couchdb/it/CouchdbTestResource.java
+++ couchdb/integration-tests/src/test/java/io/quarkiverse/jnosql/document/couchdb/it/CouchdbTestResource.java
@@ -1,12 +1,16 @@
 package io.quarkiverse.jnosql.document.couchdb.it;
 
+import static java.util.Objects.requireNonNull;
+
 import java.net.http.HttpClient;
-import java.util.Base64;
-import java.util.Map;
-import java.util.Set;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import jakarta.json.Json;
-import jakarta.json.JsonArrayBuilder;
 
 import org.eclipse.jnosql.databases.couchdb.communication.CouchDBConfigurations;
 import org.eclipse.jnosql.mapping.core.config.MappingConfigurations;
@@ -47,8 +51,9 @@ public class CouchdbTestResource implements QuarkusTestResourceLifecycleManager 
                                     CouchDBConfigurations.PASSWORD.get(),
                                     String.class));
             container.start();
-            createDatabase(config);
-            createIndexes(config);
+            LOGGER.info("CouchDB started at {}:{}", container.getHost(), container.getMappedPort(PORT));
+
+            settingUpDatabase(config);
 
             return Map.of(
                     CouchDBConfigurations.PORT.get(),
@@ -62,73 +67,96 @@ public class CouchdbTestResource implements QuarkusTestResourceLifecycleManager 
         }
     }
 
-    private void createDatabase(Config config) {
-        String username = config.getValue(CouchDBConfigurations.USER.get(), String.class);
-        String password = config.getValue(CouchDBConfigurations.PASSWORD.get(), String.class);
-        String databaseName = config.getValue(MappingConfigurations.DOCUMENT_DATABASE.get(), String.class);
+    record Settings(String user, String password, String databaseName) {
 
-        HttpClient client = HttpClient.newHttpClient();
-        String url = String.format("http://%s:%d/%s", container.getHost(),
-                container.getMappedPort(PORT), databaseName);
-
-        try {
-            client.sendAsync(
-                    java.net.http.HttpRequest.newBuilder()
-                            .uri(java.net.URI.create(url))
-                            .header("Content-Type", "application/json")
-                            .header("Authorization",
-                                    "Basic " + Base64.getEncoder()
-                                            .encodeToString("%s:%s".formatted(username, password).getBytes()))
-                            .PUT(java.net.http.HttpRequest.BodyPublishers.noBody())
-                            .build(),
-                    java.net.http.HttpResponse.BodyHandlers.ofString())
-                    .thenAccept(response -> LOGGER.info("Database Creation Result: {}", response.body()))
-                    .join();
-        } catch (Exception e) {
-            LOGGER.error("Failed to create database", e);
+        static Settings of(Config config) {
+            return new Settings(
+                    requireNonNull(config.getValue(CouchDBConfigurations.USER.get(), String.class),
+                            "CouchDB user must not be null. Please set the '%s' configuration property."
+                                    .formatted(CouchDBConfigurations.USER.get())),
+                    requireNonNull(config.getValue(CouchDBConfigurations.PASSWORD.get(), String.class),
+                            "CouchDB password must not be null. Please set the '%s' configuration property."
+                                    .formatted(CouchDBConfigurations.PASSWORD.get())),
+                    requireNonNull(config.getValue(MappingConfigurations.DOCUMENT_DATABASE.get(), String.class),
+                            "CouchDB database name must not be null. Please set the '%s' configuration property."
+                                    .formatted(MappingConfigurations.DOCUMENT_DATABASE.get())));
         }
     }
 
-    private void createIndexes(Config config) {
+    private void settingUpDatabase(Config config) {
 
-        String username = config.getValue(CouchDBConfigurations.USER.get(), String.class);
-        String password = config.getValue(CouchDBConfigurations.PASSWORD.get(), String.class);
-        String databaseName = config.getValue(MappingConfigurations.DOCUMENT_DATABASE.get(), String.class);
+        Settings settings = Settings.of(config);
 
-        createIndex(databaseName, username, password, Set.of("number"), "number_index");
-        createIndex(databaseName, username, password, Set.of("guest.document"), "guest_document_index");
+        createDatabase(settings);
+
+        createIndexes(settings);
+
+        LOGGER.info("Database '{}' created and indexes set up.", settings.databaseName());
     }
 
-    private void createIndex(String databaseName, String username, String password, Set<String> fields, String indexName) {
-        HttpClient client = HttpClient.newHttpClient();
-        String url = String.format("http://%s:%d/%s/_index", container.getHost(),
-                container.getMappedPort(PORT), databaseName);
+    private void createDatabase(Settings settings) {
+        execute(settings,
+                httpRequest -> httpRequest.PUT(java.net.http.HttpRequest.BodyPublishers.noBody()),
+                response -> LOGGER.info("Database Creation Result: {}", response.body()),
+                throwable -> LOGGER.error("Failed to create database", throwable));
 
-        JsonArrayBuilder arrayFields = Json.createArrayBuilder();
-        fields.forEach(arrayFields::add);
+    }
 
-        String indexCreationPayload = Json.createObjectBuilder()
+    private void createIndexes(Settings settings) {
+        createIndex(settings, "number_index", "number");
+        createIndex(settings, "guest_document_index", "guest.document");
+    }
+
+    private void createIndex(Settings settings, String indexName, String... fields) {
+
+        var fieldsUsedByIndex = Arrays.stream(fields)
+                .filter(Objects::nonNull)
+                .filter(Predicate.not(String::isBlank))
+                .reduce(Json.createArrayBuilder(),
+                        (arrayFields, field) -> arrayFields.add(field),
+                        (a, b) -> a.addAll(b))
+                .build();
+
+        var indexCreationPayload = Json.createObjectBuilder()
                 .add("index", Json.createObjectBuilder()
-                        .add("fields", arrayFields.build()).build())
+                        .add("fields", fieldsUsedByIndex).build())
                 .add("name", indexName)
                 .add("type", "json")
-                .build()
-                .toString();
+                .build().toString();
+
+        execute(settings,
+                httpRequest -> httpRequest.POST(java.net.http.HttpRequest.BodyPublishers.ofString(indexCreationPayload)),
+                response -> LOGGER.info("Index Creation Result: {}", response.body()),
+                throwable -> LOGGER.error("Failed to create index", throwable));
+    }
+
+    private void execute(Settings settings,
+            Function<HttpRequest.Builder, HttpRequest.Builder> httpRequestBuilderFunction,
+            Consumer<HttpResponse<String>> httpResponseConsumer,
+            Consumer<Throwable> throwableConsumer) {
+
+        HttpClient client = HttpClient.newHttpClient();
+        String url = String.format("http://%s:%d/%s", container.getHost(),
+                container.getMappedPort(PORT), settings.databaseName());
+
         try {
             client.sendAsync(
-                    java.net.http.HttpRequest.newBuilder()
-                            .uri(java.net.URI.create(url))
-                            .header("Content-Type", "application/json")
-                            .header("Authorization",
-                                    "Basic " + Base64.getEncoder()
-                                            .encodeToString("%s:%s".formatted(username, password).getBytes()))
-                            .POST(java.net.http.HttpRequest.BodyPublishers.ofString(indexCreationPayload))
+                    Objects.requireNonNull(httpRequestBuilderFunction, "httpRequestBuilderFunction must not be null")
+                            .apply(java.net.http.HttpRequest.newBuilder()
+                                    .uri(java.net.URI.create(url))
+                                    .header("Content-Type", "application/json")
+                                    .header("Authorization", "Basic " + Base64.getEncoder()
+                                            .encodeToString(
+                                                    "%s:%s".formatted(settings.user(), settings.password()).getBytes())))
                             .build(),
                     java.net.http.HttpResponse.BodyHandlers.ofString())
-                    .thenAccept(response -> LOGGER.info("Index Creation Result: {}", response.body()))
+                    .thenAccept(Objects.requireNonNull(httpResponseConsumer, "httpResponseConsumer must not be null"))
                     .join();
         } catch (Exception e) {
-            LOGGER.error("Failed to create index", e);
+            Optional.ofNullable(throwableConsumer)
+                    .ifPresentOrElse(
+                            consumer -> consumer.accept(e),
+                            () -> LOGGER.error("Something unexpected has happened!", e));
         }
     }
 
