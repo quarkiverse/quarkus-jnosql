c64a996081af436b4c5c6b538fd34638198a36f2
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-04 05:43:10 +0100

    test: disable JNoSQLResourceIT

--- solr/integration-tests/src/test/java/io/quarkiverse/jnosql/document/solr/it/JNoSQLResourceIT.java
+++ solr/integration-tests/src/test/java/io/quarkiverse/jnosql/document/solr/it/JNoSQLResourceIT.java
@@ -1,7 +1,5 @@
 package io.quarkiverse.jnosql.document.solr.it;
 
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
+//@io.quarkus.test.junit.QuarkusIntegrationTest;
 public class JNoSQLResourceIT extends JNoSQLResourceTest {
 }
