3c9a5e71efbc1d415abb4aa75e14f0681832e753
Otavio Santana <otaviopolianasantana@gmail.com>
2026-09-20 20:27:35 +0100

    build: configure gRPC and register native image services for InfluxDB extension

--- influxdb/deployment/src/main/java/io/quarkiverse/jnosql/timeseries/influxdb/deployment/Processor.java
+++ influxdb/deployment/src/main/java/io/quarkiverse/jnosql/timeseries/influxdb/deployment/Processor.java
@@ -3,11 +3,16 @@ package io.quarkiverse.jnosql.timeseries.influxdb.deployment;
 import org.eclipse.jnosql.databases.influxdb.communication.InfluxDBTimeSeriesConfiguration;
 import org.eclipse.jnosql.databases.influxdb.communication.QuarkusInfluxDBDatabaseManagerProducer;
 
+import io.grpc.LoadBalancerProvider;
+import io.grpc.NameResolverProvider;
+import io.quarkiverse.jnosql.core.deployment.ServiceProviderRegister;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.arc.deployment.ExcludedTypeBuildItem;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ServiceProviderBuildItem;
 
 class Processor {
 
@@ -30,5 +35,19 @@ class Processor {
         excludedTypeProducer.produce(
                 new ExcludedTypeBuildItem(
                         "org.eclipse.jnosql.mapping.timeseries.configuration.TimeSeriesManagerSupplier"));
+    }
+
+    @BuildStep
+    void configureNativeImage(BuildProducer<ServiceProviderBuildItem> services,
+            BuildProducer<RuntimeInitializedClassBuildItem> runtimeInitializedClasses) {
+        ServiceProviderRegister.registerService(services,
+                LoadBalancerProvider.class,
+                NameResolverProvider.class);
+        runtimeInitializedClasses.produce(
+                new RuntimeInitializedClassBuildItem("io.grpc.netty.Utils$ByteBufAllocatorPreferHeapHolder"));
+        runtimeInitializedClasses.produce(
+                new RuntimeInitializedClassBuildItem("io.grpc.netty.Utils$ByteBufAllocatorPreferDirectHolder"));
+        runtimeInitializedClasses.produce(
+                new RuntimeInitializedClassBuildItem("org.apache.arrow.memory.netty.NettyAllocationManager"));
     }
 }
